dedc1d43c378f4aa2e7d0f338eacb2fe7291a6ba
wilaxx <wilaxx59@gmail.com>
2023-08-29 07:36:02 +0200

    test generateRandom.. ok

--- src/Utils/spotify.test.js
+++ src/Utils/spotify.test.js
@@ -10,7 +10,64 @@ describe('Spotify', () => {
     });
   });
 
-  // Ajoutez d'autres tests pour les autres fonctions de Spotify ici
+  // describe('generateCodeChallenge', () => {
+  //   it('returns a hashBase64 code', () => {
+  //     const codeVerifier =;
+  //     const other_var = Spotify.function(codeVerifier);
+  //     expect().toBe();
+  //   });
+  // });
+
+  // describe('authorize', () => {
+  //   it('opens new window to spotify auth page', () => {
+  //     const variable =;
+  //     const other_var = Spotify.authorize();
+  //     expect(randomString.length).toBe(length);
+  //   });
+  // });
+
+  // describe('getAccessToken', () => {
+  //   it('returns access_token and refresh_token', () => {
+  //     const variable =;
+  //     const other_var = Spotify.getAccessToken();
+  //     expect().toBe();
+  //   });
+  // });
+
+  // describe('refreshToken', () => {
+  //   it('returns new access_toekn and refresh_token from spotify', () => {
+  //     const refToken =;
+  //     const other_var = Spotify.refreshToken(refToken);
+  //     expect().toBe();
+  //   });
+  // });
+
+  // describe('displayObject', () => {
+  //   it('displays the object returns by the fetch call', () => {
+  //     const variable =;
+  //     const other_var = Spotify.displayObject(obj);
+  //     expect().toBe();
+  //   });
+  // });
+
+  // describe('search', () => {
+  //   it('returns results as object', () => {
+  //     const variable =;
+  //     const other_var = Spotify.search(word);
+  //     expect().toBe();
+  //   });
+  // });
+
+  // describe('savePlaylist', () => {
+  //   it('generates a random string of specified length', () => {
+  //     const name = "playlist1";
+  //     const tracks = [{}, {}, {}];
+  //     const other_var = Spotify.savePlaylist(name, tracks);
+  //     expect().toBe();
+  //   });
+  // });
+
+
 });
 
 
